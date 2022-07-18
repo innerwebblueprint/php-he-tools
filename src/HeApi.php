0bc09b4ba19638c87b53b562edfd6689b057aeff
Florent Kosmala <kosflorent@gmail.com>
2022-07-18 17:24:18 +0200

    remove many tabs

--- src/HeApi.php
+++ src/HeApi.php
@@ -1,123 +1,122 @@
 <?php
+namespace FKosmala\PHPHeTools;
 
-	namespace FKosmala\PHPHeTools;
-	
-	use FKosmala\PHPHeTools\HeLayer;
-	
-	class HeApi {
-		private $HeLayer;
-		
-		public function __construct($heConfig = null) {
-			$this->HeLayer = new HeLayer($heConfig);
+use FKosmala\PHPHeTools\HeLayer;
+
+class HeApi {
+	private $HeLayer;
+
+	public function __construct($heConfig = null) {
+		$this->HeLayer = new HeLayer($heConfig);
+	}
+
+	// Methods related to the "/blockchain" endpoint
+	public function getStatus() {
+		$params = array();
+		$result = $this->HeLayer->call('getStatus', $params, '/blockchain');
+		return $result;
+	}
+
+	public function getLatestBlockInfo() {
+		$params = array();
+		$result = $this->HeLayer->call('getLatestBlockInfo', $params, '/blockchain');
+		return $result;
+	}
+
+	public function getBlockInfo($block) {
+		$params = array(
+			"blockNumber" => $block
+		);
+		$result = $this->HeLayer->call('getBlockInfo', $params, '/blockchain');
+		return $result;
+	}
+
+	public function getTransactionInfo($txid) {
+		$params = array(
+			"txid" => $txid
+		);
+		$result = $this->HeLayer->call('getTransactionInfo', $params, '/blockchain');
+		return $result;
+	}
+
+	// Delegation methods
+	public function getDelegationFrom($account, $token=null) {
+
+		if($token!=null) {
+			$token = strtoupper($token);
 		}
-		
-		// Methods related to the "/blockchain" endpoint
-		public function getStatus() {
-			$params = array();
-			$result = $this->HeLayer->call('getStatus', $params, '/blockchain');
-			return $result;
+
+		$params = array(
+			"contract" => "tokens",
+			"table" => "delegations",
+			"query" => array(
+				"from" => $account,
+				"symbol" => $token
+			),
+			"limit" => 1
+		);
+		$result = $this->HeLayer->call('find', $params);
+		return $result;
+	}
+
+	public function getDelegationTo($account, $token=null) {
+
+		if($token!=null) {
+			$token = strtoupper($token);
 		}
-		
-		public function getLatestBlockInfo() {
-			$params = array();
-			$result = $this->HeLayer->call('getLatestBlockInfo', $params, '/blockchain');
-			return $result;
-		}
-		
-		public function getBlockInfo($block) {
-			$params = array(
-				"blockNumber" => $block
-			);
-			$result = $this->HeLayer->call('getBlockInfo', $params, '/blockchain');
-			return $result;
-		}
-		
-		public function getTransactionInfo($txid) {
-			$params = array(
-				"txid" => $txid
-			);
-			$result = $this->HeLayer->call('getTransactionInfo', $params, '/blockchain');
-			return $result;
-		}
-		
-		// Delegation methods
-		public function getDelegationFrom($account, $token=null) {
-			
-			if($token!=null) {
-				$token = strtoupper($token);
-			}
-			
+
+		$params = array(
+			"contract" => "tokens",
+			"table" => "delegations",
+			"query" => array(
+				"to" => $account,
+				"symbol" => $token
+			),
+			"limit" => 1
+		);
+		$result = $this->HeLayer->call('find', $params);
+		return $result;
+	}
+
+	public function getPendingUndelegations($account, $token=null) {
+
+		if($token != null) {
 			$params = array(
 				"contract" => "tokens",
-				"table" => "delegations",
+				"table" => "pendingUndelegations",
 				"query" => array(
-					"from" => $account,
-					"symbol" => $token
+					"account" => $account,
+					"symbol" => strtoupper($token)
 				),
 				"limit" => 1
 			);
-			$result = $this->HeLayer->call('find', $params);
-			return $result;
-		}
-		
-		public function getDelegationTo($account, $token=null) {
-			
-			if($token!=null) {
-				$token = strtoupper($token);
-			}
-			
+		} else {
 			$params = array(
 				"contract" => "tokens",
-				"table" => "delegations",
-				"query" => array(
-					"to" => $account,
-					"symbol" => $token
-				),
-				"limit" => 1
-			);
-			$result = $this->HeLayer->call('find', $params);
-			return $result;
-		}
-		
-		public function getPendingUndelegations($account, $token=null) {
-			
-			if($token != null) {
-				$params = array(
-					"contract" => "tokens",
-					"table" => "pendingUndelegations",
-					"query" => array(
-						"account" => $account,
-						"symbol" => strtoupper($token)
-					),
-					"limit" => 1
-				);
-			} else {
-				$params = array(
-					"contract" => "tokens",
-					"table" => "pendingUndelegations",
-					"query" => array(
-						"account" => $account
-					),
-					"limit" => 1
-				);
-			}
-
-			$result = $this->HeLayer->call('find', $params);
-			return $result;
-		}
-		
-		// account related methods
-		public function getAccountBalance($account) {
-			$params = array(
-				"contract" => "tokens",
-				"table" => "balances",
+				"table" => "pendingUndelegations",
 				"query" => array(
 					"account" => $account
 				),
 				"limit" => 1
 			);
-			$result = $this->HeLayer->call('find', $params);
-			return $result;
 		}
-		
+
+		$result = $this->HeLayer->call('find', $params);
+		return $result;
 	}
+
+	// account related methods
+	public function getAccountBalance($account) {
+		$params = array(
+			"contract" => "tokens",
+			"table" => "balances",
+			"query" => array(
+				"account" => $account
+			),
+			"limit" => 1
+		);
+		$result = $this->HeLayer->call('find', $params);
+		return $result;
+	}
+
+}
--- src/HeLayer.php
+++ src/HeLayer.php
@@ -1,112 +1,111 @@
 <?php
+namespace FKosmala\PHPHeTools;
 
-	namespace FKosmala\PHPHeTools;
+class HeLayer {
+	// Toggle to true if you want to display the query & the result
+	private $debug = false;
 	
-	class HeLayer {
-		// Toggle to true if you want to display the query & the result
-		private $debug = false;
-		
-		// Default node for query HiveEngine API
-		private $heNode ="api.hive-engine.com/rpc";
-		
-		private $throw_exception = false;
-		
-		// Force Scheme with HTTPS
-		private $scheme = 'https://';
-		
-		public function __construct($heConfig = array()) {
-			
-			if (array_key_exists('debug', $heConfig)) {
-				$this->debug = $heConfig['debug'];
-			}
-			
-			if (array_key_exists('heNode', $heConfig)) {
-				$this->heNode = $heConfig['heNode'];
-			}
-			
-			if (array_key_exists('throw_exception', $heConfig)) {
-				$this->throw_exception = $heConfig['throw_exception'];
-			}
-		}
-		
-		public function getRequest($method, $contract, $table, $query, $limit = 1) {
-			$request = array(
-				"id" => 1,
-				"jsonrpc" => "2.0",
-				"method" => $method,
-				"params" => array(
-					"contract" => $contract,
-					"table" => $table,
-					"query" => $query,
-				),
-				"limit" => $limit
-			);
-				
-			$request_json = json_encode($request);
-			
-			if ($this->debug) {
-				echo "<pre>request_json<br/>".$request_json."\n</pre>";
-			}
-			
-			return $request_json;
-		}
-		
-		public function getRPCRequest($method, $params=array()) {
-			$request = array(
-				"id" => 1,
-				"jsonrpc" => "2.0",
-				"method" => $method,
-				"params" => $params
-			);
-				
-			$request_json = json_encode($request);
-			
-			if ($this->debug) {
-				echo "<pre>request_json<br/>".$request_json."\n</pre>";
-			}
-			
-			return $request_json;
-		}
-		
-		public function curl($data, $endpoint) {
-			$ch = curl_init();
-			
-			curl_setopt($ch, CURLOPT_URL, $this->scheme.$this->heNode.$endpoint);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-			
-			$result = curl_exec($ch);
-
-			if ($this->debug) {
-				echo "<pre><br>Result :<br>".$result."</pre>\n";
-			}
-
-			return $result;
-		}
+	// Default node for query HiveEngine API
+	private $heNode ="api.hive-engine.com/rpc";
 	
-		public function call($method, $params = array(), $endpoint="/contracts") {
-			if (!empty($params['contract'])) {
-				$contract = $params['contract'];
-				$table = $params['table'];
-				$query = $params['query'];
-				$limit = $params['limit'];
-				$request = $this->getRequest($method, $contract, $table, $query, $limit);
-			}
-			else {
-				$request = $this->getRPCRequest($method, $params);
-			}
-			$response = $this->curl($request, $endpoint);
-			$response = json_decode($response, true);
-			
-			if (empty($response['result'])) {
-				if ($this->throw_exception) {
-					throw new Exception('Error retrieve HiveEngine API query');
-				} else {
-					return $response['result'];
-				}
-			}
-			
-			return $response['result'];
+	private $throw_exception = false;
+	
+	// Force Scheme with HTTPS
+	private $scheme = 'https://';
+	
+	public function __construct($heConfig = array()) {
+		
+		if (array_key_exists('debug', $heConfig)) {
+			$this->debug = $heConfig['debug'];
+		}
+		
+		if (array_key_exists('heNode', $heConfig)) {
+			$this->heNode = $heConfig['heNode'];
+		}
+		
+		if (array_key_exists('throw_exception', $heConfig)) {
+			$this->throw_exception = $heConfig['throw_exception'];
 		}
 	}
+	
+	public function getRequest($method, $contract, $table, $query, $limit = 1) {
+		$request = array(
+			"id" => 1,
+			"jsonrpc" => "2.0",
+			"method" => $method,
+			"params" => array(
+				"contract" => $contract,
+				"table" => $table,
+				"query" => $query,
+			),
+			"limit" => $limit
+		);
+			
+		$request_json = json_encode($request);
+		
+		if ($this->debug) {
+			echo "<pre>request_json<br/>".$request_json."\n</pre>";
+		}
+		
+		return $request_json;
+	}
+	
+	public function getRPCRequest($method, $params=array()) {
+		$request = array(
+			"id" => 1,
+			"jsonrpc" => "2.0",
+			"method" => $method,
+			"params" => $params
+		);
+			
+		$request_json = json_encode($request);
+		
+		if ($this->debug) {
+			echo "<pre>request_json<br/>".$request_json."\n</pre>";
+		}
+		
+		return $request_json;
+	}
+	
+	public function curl($data, $endpoint) {
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $this->scheme.$this->heNode.$endpoint);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		
+		$result = curl_exec($ch);
+
+		if ($this->debug) {
+			echo "<pre><br>Result :<br>".$result."</pre>\n";
+		}
+
+		return $result;
+	}
+
+	public function call($method, $params = array(), $endpoint="/contracts") {
+		if (!empty($params['contract'])) {
+			$contract = $params['contract'];
+			$table = $params['table'];
+			$query = $params['query'];
+			$limit = $params['limit'];
+			$request = $this->getRequest($method, $contract, $table, $query, $limit);
+		}
+		else {
+			$request = $this->getRPCRequest($method, $params);
+		}
+		$response = $this->curl($request, $endpoint);
+		$response = json_decode($response, true);
+		
+		if (empty($response['result'])) {
+			if ($this->throw_exception) {
+				throw new Exception('Error retrieve HiveEngine API query');
+			} else {
+				return $response['result'];
+			}
+		}
+		
+		return $response['result'];
+	}
+}
